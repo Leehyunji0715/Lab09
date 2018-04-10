71d750fb41fea6d90e8ab4d59378b242790e0503
leehyunji0715@gmail.com <leehyunji0715@172.17.157.24>
2018-04-10 11:34:43 +0900

    SalesAssociate javadoc and line comment

--- src/main/java/edu/handong/csee/lab09/salesreport/SalesAssociate.java
+++ src/main/java/edu/handong/csee/lab09/salesreport/SalesAssociate.java
@@ -1,25 +1,31 @@
 package edu.handong.csee.lab09.salesreport;
 /**
- * 
- * 
- * 
+ * This class defines sales associate's name and how much they earn
+ * This class has mName and mSales members
+ * mName is private modifier and String class type and defines sales associate's name
+ * mSales is private modifier and double type. It has value of how much they earn
+ * This class has getmName(), setmName(), getmSales() and setmSales() methods
+ * getmName() delivers current String 
+ * setmName(), user can change mName to which user want
+ * getmSales() delivers current value of sales
+ * setmSales(), user can change mSales to which user want
  * @author leehyunji0715
  *
  */
-public class SalesAssociate {
-	private String mName;
-	private double mSales;
+public class SalesAssociate {//This is public class named SalesAssociate
+	private String mName;// call private modifier and String class type named mName 
+	private double mSales;// call private modifier and double type named mSales
 	
-	public String getmName() {
-		return mName;
+	public String getmName() {//call public method that return String
+		return mName;//return current mName value
 	}
-	void setmName(String string) {
-		this.mName = string;
+	void setmName(String string) {//call public method which returns nothing and input String user wants to store
+		this.mName = string;// save String value to mName
 	}
-	public double getmSales() {
-		return mSales;
+	public double getmSales() {//call public method which returns nothing
+		return mSales;//return current mSales value
 	}
-	public void setmSales(double sales) {
-		this.mSales = sales;
+	public void setmSales(double sales) {//return nothing and input sales value
+		this.mSales = sales;//save sales value to mSales
 	}
 }
