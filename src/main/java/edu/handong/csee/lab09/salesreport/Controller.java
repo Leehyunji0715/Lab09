0f38c14dd0370ac6fbf04da42294b19951fa8d3c
leehyunji0715@gmail.com <leehyunji0715@172.17.157.24>
2018-04-10 19:21:26 +0900

    Controller's javadoc and line comment

--- src/main/java/edu/handong/csee/lab09/salesreport/Controller.java
+++ src/main/java/edu/handong/csee/lab09/salesreport/Controller.java
@@ -1,17 +1,18 @@
 package edu.handong.csee.lab09.salesreport;
 /**
- * 
+ * This class is for operating SalesAssociate and SalesReporter class
+ * This class has main() method
  * 
  * @author leehyunji0715
  *
  */
-public class Controller {
+public class Controller {//This class is public modifier named Controller
 
-	public static void main(String[] args) {
-		SalesReporter salesReport = new SalesReporter();
-		salesReport.getData();
-		salesReport.computeStats();
-		salesReport.displayResults();
+	public static void main(String[] args) {//use main()
+		SalesReporter salesReport = new SalesReporter();//instantiate SalesReporter class naming salesReport
+		salesReport.getData();//operate getData()
+		salesReport.computeStats();//operate computeStats()
+		salesReport.displayResults();//operate diplayResults()
 	}
 
 }
